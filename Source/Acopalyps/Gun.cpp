e78388db843046ffeb98dc8a1a9e3491e09543a9
Joakim Pettersson <joakimbjorn.pettersson@gmail.com>
2023-04-17 14:21:12 +0200

    Rapid fire ammotype, kopplat ammo till HUD, Text i hud uppdateras ej i retainerbox... Inputactions för ny ammotype.
    Fixat lite ammo counting bugs

--- Source/Acopalyps/Gun.cpp
+++ Source/Acopalyps/Gun.cpp
@@ -101,6 +101,36 @@ void AGun::AlternateFire()
 	}
 }
 
+/** Rapid Fire through regular barrel.*/
+void AGun::RapidFire()
+{
+	UE_LOG(LogTemp,Display,TEXT("Trying to fire rapidly"));
+	if (Character == nullptr || Character->GetController() == nullptr)
+	{
+		return;
+	}
+
+	if(CurrentAmmoType != AMMO_TYPES::Rapid)
+	{
+		return;
+	}
+	
+	FHitResult Hit;
+	FVector ShotDirection;
+
+	if(RapidMag > 0)
+	{
+		FireRapid(Hit, ShotDirection);
+	}
+	
+	AController* OwnerController = GetOwnerController();
+	if(OwnerController == nullptr)
+	{
+		return;
+	}
+}
+
+
 /** Snap weapon to player character 0 */
 void AGun::AttachWeaponInputs(AAcopalypsCharacter* TargetCharacter)
 {
@@ -123,6 +153,7 @@ void AGun::AttachWeaponInputs(AAcopalypsCharacter* TargetCharacter)
 		{
 			// Fire
 			EnhancedInputComponent->BindAction(FireAction, ETriggerEvent::Triggered, this, &AGun::Fire);
+			EnhancedInputComponent->BindAction(RapidFireAction, ETriggerEvent::Triggered, this, &AGun::RapidFire);
 			EnhancedInputComponent->BindAction(AlternativeFireAction, ETriggerEvent::Triggered, this, &AGun::AlternateFire);
 			// Reload
 			EnhancedInputComponent->BindAction(ReloadAction, ETriggerEvent::Triggered, this, &AGun::Reload);
@@ -131,6 +162,7 @@ void AGun::AttachWeaponInputs(AAcopalypsCharacter* TargetCharacter)
 			EnhancedInputComponent->BindAction(ChangeAmmoExplosiveAction, ETriggerEvent::Triggered, this, &AGun::SetAmmoExplosive);
 			EnhancedInputComponent->BindAction(ChangeAmmoFlareAction, ETriggerEvent::Triggered, this, &AGun::SetAmmoFlare);
 			EnhancedInputComponent->BindAction(ChangeAmmoPiercingAction, ETriggerEvent::Triggered, this, &AGun::SetAmmoPiercing);
+			EnhancedInputComponent->BindAction(ChangeAmmoRapidAction, ETriggerEvent::Triggered, this, &AGun::SetAmmoRapid);
 		}
 	}
 }
@@ -191,10 +223,14 @@ void AGun::Reload()
 	case Regular:
 		// Load regular ammo if has
 		Total = *(Character->GetAmmoCountMap()->Find(Regular));
-		if( Total <= 12 )
+		if(Total == 0)
+		{
+			return;
+		}
+		if( Total + RegularMag <= 12 )
 		{
 			Character->GetAmmoCountMap()->Emplace(Regular, 0);
-			SetRegularMag(Total);
+			SetRegularMag(Total+RegularMag);
 		} else
 		{
 			Character->GetAmmoCountMap()->Emplace(Regular,(Total - 12) + RegularMag);
@@ -203,16 +239,42 @@ void AGun::Reload()
 		break;
 	case Piercing:
 		Total = *(Character->GetAmmoCountMap()->Find(Piercing));
-		if( Total <= 12 )
+		if(Total==0)
+		{
+			return;
+		}
+		if( Total + PiercingMag <= 12 )
 		{
 			Character->GetAmmoCountMap()->Emplace(Piercing, 0);
-			SetPiercingMag(Total);
+			SetPiercingMag(Total+PiercingMag);
 		} else
 		{
 			Character->GetAmmoCountMap()->Emplace(Piercing,(Total - 12) + PiercingMag);
 			SetPiercingMag(12);
 		}
 		break;
+	case Rapid:
+		//Load Rapid Fire ammo if available
+		Total = *(Character->GetAmmoCountMap()->Find(Rapid));
+		if(Total == 0)
+		{
+			//Ladda med 0 i reserv
+			return;
+		}
+		
+		if((Total+RapidMag)<=12)
+		{
+			Character->GetAmmoCountMap()->Emplace(Rapid, 0);
+			SetRapidMag(Total + RapidMag);
+		}
+
+		else
+		{
+			Character->GetAmmoCountMap()->Emplace(Rapid, (Total-12) + RapidMag);
+			SetRapidMag(12);
+		}
+		
+		
 	default:break;
 	}
 }
@@ -267,6 +329,12 @@ void AGun::SetFlareMag(int32 Size)
 {
 	FlareMag = Size;
 }
+void AGun::SetRapidMag(int32 Size)
+{
+	RapidMag = Size;
+}
+
+
 void AGun::SetAmmoRegular()
 {
 	CurrentAmmoType=AMMO_TYPES::Regular;
@@ -283,6 +351,13 @@ void AGun::SetAmmoPiercing()
 {
 	CurrentAmmoType=AMMO_TYPES::Piercing;
 }
+void AGun::SetAmmoRapid()
+{
+	CurrentAmmoType=AMMO_TYPES::Rapid;
+	UE_LOG(LogTemp,Display,TEXT("Ammo swapped"));
+}
+
+
 AMMO_TYPES AGun::GetCurrentAmmoType()
 {
 	return CurrentAmmoType;
@@ -483,6 +558,80 @@ void AGun::FirePiercing(FHitResult& Hit, FVector& ShotDirection)
 		}
 	}
 }
+
+void AGun::FireRapid(FHitResult& Hit, FVector& ShotDirection)
+{
+	UE_LOG(LogTemp,Display,TEXT("Fire rapidly"));
+	// Decrease ammo count by 1
+	RapidMag--;
+	if(GunTraceInaccurate(Hit, ShotDirection))
+	{
+		DrawDebugSphere(GetWorld(),Hit.Location,10,10,FColor::Orange,true,5);
+		AActor* HitActor = Hit.GetActor();
+		if(HitActor != nullptr)
+		{
+			if (ImpactSoundRegularAmmo != nullptr)
+			{
+				UGameplayStatics::PlaySoundAtLocation(this, ImpactSoundRegularAmmo, Hit.Location);
+			}
+			if (ImpactEffectRegularAmmo != nullptr)
+			{
+				UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), ImpactEffectRegularAmmo, Hit.Location, ShotDirection.Rotation());
+			}
+			FPointDamageEvent DamageEvent(Damage, Hit, ShotDirection, nullptr);
+			HitActor->TakeDamage(Damage, DamageEvent, GetOwnerController(), GetOwner());;
+		}
+		FireTriggerEvent(Hit, ShotDirection);
+	}
+	
+	// Try and play the sound if specified
+	if (FireSound != nullptr)
+	{
+		UGameplayStatics::PlaySoundAtLocation(this, FireSound, Character->GetActorLocation());
+	}
+	
+	// Try and play a firing animation if specified
+	if (FireAnimation != nullptr)
+	{
+		// Get the animation object for the arms mesh
+		UAnimInstance* AnimInstance = Character->GetMesh1P()->GetAnimInstance();
+		if (AnimInstance != nullptr)
+		{
+			AnimInstance->Montage_Play(FireAnimation, 1.f);
+		}
+	}
+}
+
+bool AGun::GunTraceInaccurate(FHitResult& HitResult, FVector& ShotDirection)
+{
+	AController* OwnerController = GetOwnerController();
+	if(OwnerController == nullptr)
+	{
+		return false;
+	}
+	
+	FVector Location;
+	FRotator Rotation;
+	OwnerController->GetPlayerViewPoint(Location, Rotation);
+	Rotation = RandomRotator(Rotation.Pitch,Rotation.Yaw,Rotation.Roll, InaccuracyModifier);
+	ShotDirection = -Rotation.Vector();
+	FVector End = Location + Rotation.Vector()*MaxRange;
+	
+	FCollisionQueryParams Params;
+	Params.AddIgnoredActor(this);
+	Params.AddIgnoredActor(Character);
+	return GetWorld()->LineTraceSingleByChannel(HitResult,Location,End,ECollisionChannel::ECC_GameTraceChannel1, Params);
+}
+
+FRotator AGun::RandomRotator(float Pitch, float Yaw, float Roll, float Interval) const
+{
+	const float NewPitch = FMath::FRandRange(Pitch,Pitch+Interval);
+	const float NewYaw = FMath::FRandRange(Yaw-Interval,Yaw+Interval);
+	//const float NewRoll = FMath::FRandRange(Roll-Interval,Roll+Interval);
+	return FRotator(NewPitch,NewYaw,Roll);
+}
+
+
 
 
 
